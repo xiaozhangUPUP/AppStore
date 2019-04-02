e17b1dfda00e93ac8af6849ffa7db5822ecee8f2
zhangqi <969860880@qq.com>
2019-04-02 23:45:47 +0800

    add 1.固定view的位置;2.view向上向下展开;

--- app/src/main/java/com/appstore/android/ui/activity/AppDetailActivity.java
+++ app/src/main/java/com/appstore/android/ui/activity/AppDetailActivity.java
@@ -1,12 +1,20 @@
 package com.appstore.android.ui.activity;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.view.View;
-import android.widget.ImageView;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 
 import com.appstore.android.R;
+import com.appstore.android.common.util.DensityUtil;
 import com.appstore.android.di.component.AppComponent;
 
 import butterknife.BindView;
@@ -15,10 +23,8 @@ import butterknife.ButterKnife;
 public class AppDetailActivity extends BaseActivity {
 
 
-    @BindView(R.id.img_view)
-    ImageView imgView;
-    @BindView(R.id.activity_appdetail)
-    LinearLayout activityAppdetail;
+    @BindView(R.id.view_content)
+    FrameLayout viewContent;
 
     @Override
     public int setLayout() {
@@ -30,13 +36,60 @@ public class AppDetailActivity extends BaseActivity {
 
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
     @Override
     public void init() {
         View view = myApplication.getView();
         Bitmap viemBitmapCache = getViemBitmapCache(view);
         if (viemBitmapCache != null) {
-            imgView.setImageBitmap(viemBitmapCache);
+            viewContent.setBackground(new BitmapDrawable(viemBitmapCache));
         }
+
+        int[] outLocation = new int[2];
+
+        // 拿到原来view的坐标
+        view.getLocationOnScreen(outLocation);
+
+        int left = outLocation[0];
+        int top = outLocation[1];
+
+        int statusBarH = DensityUtil.getStatusBarH(this);
+
+        ViewGroup.MarginLayoutParams marginLayoutParams = new ViewGroup.MarginLayoutParams(view.getLayoutParams());
+        marginLayoutParams.leftMargin = left;
+        marginLayoutParams.topMargin = top - statusBarH;
+        marginLayoutParams.width = view.getWidth();
+        marginLayoutParams.height = view.getHeight();
+        int dif = DensityUtil.getScreenH(this) - top;
+        if (dif < view.getHeight()) {
+//            marginLayoutParams.topMargin = DensityUtil.getScreenH(this) - view.getHeight() - statusBarH;
+            marginLayoutParams.topMargin = top - statusBarH - (view.getHeight() - dif);
+        }
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(marginLayoutParams);
+
+        // 重新设置view的位置
+        viewContent.setLayoutParams(layoutParams);
+        open();
+    }
+
+    private void open() {
+        int h = DensityUtil.getScreenH(this);
+        ObjectAnimator animator = ObjectAnimator.ofFloat(viewContent, "scaleY", 1f, h);
+        animator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                viewContent.setBackgroundColor(getResources().getColor(R.color.white));
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+
+            }
+
+        });
+        animator.setStartDelay(2000);
+        animator.setDuration(10000);
+        animator.start();
     }
 
     private Bitmap getViemBitmapCache(View view) {
